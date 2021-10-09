e6e472944d3c6a83163158ffb0cd236cdc16e3b2
Nipun Mahajan <71063756+nipun0307@users.noreply.github.com>
2021-10-09 15:41:57 +0530

    Added __str__ to class 5,6

--- my_project_site/project_allocation/models.py
+++ my_project_site/project_allocation/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
-
+# Class - 1
 class Instructor(models.Model):
     instructor_name = models.CharField(max_length=40)
 
@@ -9,7 +9,7 @@ class Instructor(models.Model):
         return str(self.id)
     
 
-
+# Class - 2
 class Student(models.Model):
     student_roll_num = models.IntegerField(primary_key=True, validators=[MinValueValidator(1)])
     student_name = models.CharField(max_length=40)
@@ -18,7 +18,7 @@ class Student(models.Model):
         return str(self.student_roll_num)
 
 
-
+# Class - 3
 class Course(models.Model):
     course_name = models.CharField(max_length=100)
     course_code = models.SlugField(max_length=30, default="course-code-not-applicable")
@@ -29,7 +29,7 @@ class Course(models.Model):
         return str(self.id)
 
 
-
+# Class - 4
 class Project(models.Model):
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_id_project')
     project_name = models.CharField(max_length=200)
@@ -40,18 +40,29 @@ class Project(models.Model):
     def __str__(self):
         return (self.id)
 
+
+# Class - 5
 class Peer_edges (models.Model):
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_id')
     student_roll_num = models.ForeignKey (Student, on_delete=models.CASCADE, related_name='student_roll_num')
     peer_roll_num = models.ForeignKey (Student, on_delete=models.CASCADE, related_name='peer_roll_num')
     status = models.SlugField(max_length=1, default="N")   
     # N - Neutral , F - Friends , E - enemy
+    def __str__ (self):
+        return str(self.course_id) + "\t:\t(" + str(self.student_roll_num) + " , " + str(self.peer_roll_num) + ")\t:\t" + str(self.status)
 
+
+# Class - 6
 class Projects_pref (models.Model):
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_id')
     student_roll_num = models.ForeignKey (Student, on_delete=models.CASCADE, related_name='student_roll_num')
     project_id = models.ForeignKey (Project, on_delete=models.CASCADE, related_name= 'project_id')
 
+    def __str__ (self):
+        return str(self.course_id) + "\t:\t" + str(self.student_roll_num) + "\t:\t" + str(self.project_id)
+
+
+# Class - 7
 class Student_Enrollment(models.Model):
     student_roll_num = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='student_roll_enrolled')
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_id_enrolled')
