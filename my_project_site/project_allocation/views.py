a50f8e4d78cd15467e284baf47062e9eb713c050
Nipun Mahajan <71063756+nipun0307@users.noreply.github.com>
2021-10-20 02:10:32 +0530

    Hypertext added for instructor redirection

--- my_project_site/project_allocation/views.py
+++ my_project_site/project_allocation/views.py
@@ -58,58 +58,67 @@ def choose (request):
 # ###########################################################################################
 
 def instructor_index(request):
-    courses = Course.objects.all()
+    user = request.user
+    if user.is_authenticated:
+        if Instructor.objects.filter(intructor_email = request.user.email).exists():
+            courses = Course.objects.all()
 
-    enrollments = {}
-    projects = {}
-    pub = {}
+            enrollments = {}
+            projects = {}
+            pub = {}
 
-    for course in courses:
-        row = Student_Enrollment.objects.filter(course_id = course.id)
-        enrollments[course.id] = len(row)
-        row = Project.objects.filter(course_id = course.id)
-        projects[course.id] = len(row)
-        pub[course.id] = course.is_pub
+            for course in courses:
+                row = Student_Enrollment.objects.filter(course_id = course.id)
+                enrollments[course.id] = len(row)
+                row = Project.objects.filter(course_id = course.id)
+                projects[course.id] = len(row)
+                pub[course.id] = course.is_pub
 
-    context = {
-        'courses': courses, 
-        'enrollments': enrollments,
-        'projects': projects,
-        'pub': pub,
-    }
+            context = {
+                'courses': courses, 
+                'enrollments': enrollments,
+                'projects': projects,
+                'pub': pub,
+            }
 
-    return render(request, 'project_allocation/instructor_index.html',context)
+            return render(request, 'project_allocation/instructor_index.html',context)
+    response = redirect('/project_allocation/logout/')
+    return response
 
 # ###########################################################################################
 # ###########################################################################################
 
 def instructor_course(request, course_id):
+    user = request.user
+    if user.is_authenticated:
+        if Instructor.objects.filter(intructor_email = request.user.email).exists():
+            course = Course.objects.get(pk=course_id)
 
-    course = Course.objects.get(pk=course_id)
+            added = False
+            if request.method == "POST":
+                form = AddProjectToListForm(request.POST)
+                if form.is_valid():
+                    p = course.course_id_project.create(project_name = form.cleaned_data['project_name'], 
+                                project_description = form.cleaned_data['project_description'],
+                                team_size = form.cleaned_data['team_size'],
+                                num_teams = form.cleaned_data['num_teams'])
+                    p.save()
+                    return HttpResponseRedirect('/project_allocation/instructor/'+str(course_id))
+            else:
+                form = AddProjectToListForm  
+                    
+            projects = Project.objects.filter(course_id = course_id, ).order_by('id').reverse()
 
-    added = False
-    if request.method == "POST":
-        form = AddProjectToListForm(request.POST)
-        if form.is_valid():
-            p = course.course_id_project.create(project_name = form.cleaned_data['project_name'], 
-                        project_description = form.cleaned_data['project_description'],
-                        team_size = form.cleaned_data['team_size'],
-                        num_teams = form.cleaned_data['num_teams'])
-            p.save()
-            return HttpResponseRedirect('/project_allocation/instructor/'+str(course_id))
-    else:
-        form = AddProjectToListForm  
-            
-    projects = Project.objects.filter(course_id = course_id, ).order_by('id').reverse()
+            context = {
+                'course': course, 
+                'projects': projects,
+                'form': form,
+                'added': added,
+            }
 
-    context = {
-        'course': course, 
-        'projects': projects,
-        'form': form,
-        'added': added,
-    }
-
-    return render(request, 'project_allocation/instructor_course.html',context)
+            return render(request, 'project_allocation/instructor_course.html',context)
+    response = redirect('/project_allocation/logout/')
+    return response
 
 # ###########################################################################################
 # ###########################################################################################
